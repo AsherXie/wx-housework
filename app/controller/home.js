69d4aa6835a701d1cda9e3884b701da7b81c149c
Asher <asher@AsherdeMacBook-Pro.local>
2020-03-01 16:16:01 +0800

    修复上传

--- app/controller/home.js
+++ app/controller/home.js
@@ -19,14 +19,13 @@ class HomeController extends Controller {
     }
     async HouseworkUpload() {
         const ctx = this.ctx;
-        console.log()
         // console.log()
         //egg-multipart 已经帮我们处理文件二进制对象
         // node.js 和 php 的上传唯一的不同就是 ，php 是转移一个 临时文件
         // node.js 和 其他语言（java c#） 一样操作文件流
         const stream = await ctx.getFileStream();
         //新建一个文件名
-        const filename = `${ctx.request.headers.name} ${moment().format('YYYY-MM-DD HH:mm:ss')}.jpg`
+        const filename = `${decodeURI(ctx.request.headers.name)} ${moment().format('YYYY-MM-DD HH:mm:ss')}.jpg`
         //文件生成绝对路径
         //当然这里这样市不行的，因为你还要判断一下是否存在文件路径
         const target = path.join(this.config.baseDir, 'app/public/uploads', filename);
